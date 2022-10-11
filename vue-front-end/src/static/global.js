50dd000a7c0b2a0d3f3f3d240b55fb11d0389deb
aumartines <acm_asd@hotmail.com>
2022-10-11 12:06:31 -0600

    Build

--- vue-front-end/src/static/global.js
+++ vue-front-end/src/static/global.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://vue-laravel-datatables.herokuapp.com/api/'
-const BASE_URL = 'http://localhost:8000/api/'
+const BASE_URL = 'https://vue-laravel-datatables.herokuapp.com/api/'
+// const BASE_URL = 'http://localhost:8000/api/'
 
 export const URL = {
   GET_API: BASE_URL + 'data',
